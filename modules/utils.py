d981af9f901619c2984930b95944d76289e7410d
jhinter <37086141+jhinter@users.noreply.github.com>
2019-10-06 12:58:51 +0200

    Update utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -1,5 +1,4 @@
 import requests
-import pprint
 from tinydb import TinyDB, Query
 
 def get_new_session():
@@ -10,17 +9,3 @@ def get_db_table():
     db = TinyDB('./articles.json')
     table = db.table('posts')
     return table
-    
-def persist_new_articles(articles):
-    table = get_db_table()
-    for article in articles:
-        if table.contains(Query().title == str(article["title"])) == False:
-            table.insert({
-                'title': article["title"],
-                'description': article["description"],
-                'link': article["link"],
-                'sent': False
-            })
-            print('New article added: ' + article["title"])
-        else:
-            print('Article already exists: ' + article["title"])
