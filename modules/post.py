a05093a072acda6b3cf31f4b1b588d95c47d136e
jhinter <37086141+jhinter@users.noreply.github.com>
2019-10-06 12:57:17 +0200

    Update post.py

--- modules/post.py
+++ modules/post.py
@@ -8,17 +8,17 @@ def post_new_articles():
 
     for article in table.search(Query().sent == False):
         post = {
-            'message': None,
+            'message': 'New deal!',
             'link': article.get('link'),
         }
         graph = facebook.GraphAPI(access_token="YOUR_TOKEN_HERE", version="3.1")
         
-        success = graph.put_object(
+        api_request = graph.put_object(
             parent_object='YOUR_PAGE_HERE',
             connection_name='feed',
             message=post['message'],
             link=post['link']
         )
 
-        if success:
+        if api_request == True:
             table.update({'sent': True}, Query().link == post['link'])
