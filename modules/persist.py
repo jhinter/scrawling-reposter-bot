c388db9c59b84fa3ca41265f4b863bb7adb406f6
jhinter <37086141+jhinter@users.noreply.github.com>
2019-10-06 13:01:26 +0200

    Create persist.py

---
+++ modules/persist.py
@@ -0,0 +1,16 @@
+from tinydb import TinyDB, Query
+from utils import get_db_table
+
+def persist_new_articles(articles):
+    table = get_db_table()
+    for article in articles:
+        if table.contains(Query().title == str(article["title"])) == False:
+            table.insert({
+                'title': article["title"],
+                'description': article["description"],
+                'link': article["link"],
+                'sent': False
+            })
+            print('New article added: ' + article["title"])
+        else:
+            print('Article already exists: ' + article["title"])
